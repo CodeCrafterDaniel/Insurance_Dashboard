7d5ae25ef402a05f6f7575126bf631db946fdc35
DanilFedotov <d.fedotov@edu.centraluniversity.ru>
2026-06-03 11:37:27 +0300

    Updated graphs

--- graphs/volume_graph.py
+++ graphs/volume_graph.py
@@ -2,7 +2,6 @@ import pandas as pd
 import plotly.graph_objects as go
 
 def create_volume_graph(df_volume):
-    df_volume = pd.read_excel('Статистика_по_рынку_Банк_России.xlsx', sheet_name='Объем')
 
     df_volume["Премии (за квартал)"] = df_volume["Премии (за квартал)"] / 1000
     df_volume["Выплаты (за квартал)"] = df_volume["Выплаты (за квартал)"] / 1000
@@ -60,7 +59,8 @@ def create_volume_graph(df_volume):
         yaxis2=dict(
             title="Прирост %",
             overlaying="y",
-            side="right"
+            side="right",
+            showgrid=False
         ),
 
         legend=dict(
